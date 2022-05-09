4917f9a709294e735ecef2b244e9b3fdd9525712
Javier <javier.zen@gmail.com>
2022-05-10 00:27:38 +0200

    Create AkashBoard.vue

---
+++ src/components/AkashBoard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <app-bar />
+  </div>
+</template>
+
+<script>
+import AppBar from './AppBar.vue'
+
+export default {
+  name: 'AkashBoard',
+  components: {
+    AppBar
+  }
+}
+</script>
