05ebbd8824a99320eb72d53334c698281f7f6da0
Javier <javier.zen@gmail.com>
2022-05-09 21:48:12 +0200

    Update App.vue
    Delete Hello World

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <akash-board />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AkashBoard from './components/AkashBoard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AkashBoard
   }
 }
 </script>
