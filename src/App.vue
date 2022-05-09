f3e331fe31aa1f8dbab7effcf15c94d134c4f9c5
Javier <javier.zen@gmail.com>
2022-05-10 00:28:49 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,63 @@
 <template>
-  <div id="app">
-    <akash-board />
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="white"
+      flat
+    >
+
+    </v-app-bar>
+
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            sm="4"
+          >
+            <v-sheet
+              rounded="lg"
+              min-height="268"
+            >
+              <v-img
+                contain
+                max-height="80%"
+                max-width="100%"
+                src="https://picsum.photos/id/11/500/300"
+              ></v-img>
+
+            </v-sheet>
+          </v-col>
+
+          <v-col
+            cols="12"
+            sm="8"
+          >
+            <v-sheet
+              rounded="lg"
+              min-height="268"
+            >
+              <sketch-test />
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import AkashBoard from './components/AkashBoard.vue'
+import SketchTest from './components/SketchTest.vue';
 
 export default {
   name: 'App',
-  components: {
-    AkashBoard
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    SketchTest
+  },
+
+  data: () => ({
+
+  })
+};
+</script>
